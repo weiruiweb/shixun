b7b97072dd24448d315e8119d97b5d507e9ce0ee
weiruiweb <40764254+weiruiweb@users.noreply.github.com>
2018-08-27 19:55:34 +0800

    修改导航1

--- js/my.js
+++ js/my.js
@@ -14,11 +14,73 @@ $(function(){
 		$(this).find('i').stop().animate({left:'50%',width:0},200);	
 	})
 
+});
+$(window).load(function() {
+    
 
-	
+     $('.video').waypoint(function(direction) {
+
+        if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
+
+            $('.video .process_item').each(function(k) {
+                var el = $(this);
+
+                setTimeout(function() {
+                    el.addClass('fadeInDown animated');
+                }, k * 200, 'easeInOutExpo');
+
+            });
+
+        }
+    }, {
+        offset: '600px'
+
+    });
+     $('.product').waypoint(function(direction) {
+
+        if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
+
+            $('.product_cont').each(function(k) {
+                var el = $(this);
+
+                setTimeout(function() {
+                    el.addClass('fadeInLeft animated');
+                }, 1 * 100, 'easeInOutExpo');
+
+            });
+        $('.product_cont1').each(function(k) {
+                        var el = $(this);
+
+                        setTimeout(function() {
+                            el.addClass('fadeInRight animated1');
+                        }, 1 * 100, 'easeInOutExpo');
+
+                    });
+
+        }
+    }, {
+        offset: '200px'
+
+    });
+
+     $('.case').waypoint(function(direction) {
+
+        if (direction === 'down' && !$(this.element).hasClass('animated')) {
+    
+           $('.caseLeft').addClass('fadeInLeft animated');  
+           $('.caseRight').addClass('fadeInRight animated');
+          
+        }
+    }, {
+        offset: '100px'
+
+    });
 
 
 });
+
 
 
 	
