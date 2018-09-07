4e7481a11305cd68c0ecbba6312465656ee37088
weiruiweb <40764254+weiruiweb@users.noreply.github.com>
2018-09-07 18:31:42 +0800

    commit

--- js/base.js
+++ js/base.js
@@ -141,8 +141,33 @@ window.base={
         return form; 
     },
     
-    getDataSet:function(e) {   
+    /*getDataSet:function(e) {   
         return e.target.dataset; 
+    },*/
+
+
+    getDataset:function(ele){
+        if(ele.dataset){
+            return ele.dataset;
+        }else{
+            var attrs = ele.attributes,//元素的属性集合
+                dataset = {},
+                name,
+                matchStr;
+
+            for(var i = 0;i<attrs.length;i++){
+                //是否是data- 开头
+                matchStr = attrs[i].name.match(/^data-(.+)/);
+                if(matchStr){
+                    //data-auto-play 转成驼峰写法 autoPlay
+                    name = matchStr[1].replace(/-([\da-z])/gi,function(all,letter){
+                        return letter.toUpperCase();
+                    });
+                    dataset[name] = attrs[i].value;
+                }
+            }
+            return dataset;
+        }
     },
 
     findKeyFromArray:function(Array,key,value) {  
